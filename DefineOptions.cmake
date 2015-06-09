97345c9b19e2e94439080c0e161034a12451433a
Zeev <zeev@debtop>
2015-06-09 21:49:45 +0300

    Updating dependencies

--- DefineOptions.cmake
+++ DefineOptions.cmake
@@ -1,5 +1,5 @@
 option(BUILD_SHARED_LIBS "Build shared libraries" ON)
-option(WITH_MATIO "Build with MATIO (Matlab) support" OFF)
+option(WITH_MATIO "Build with MATIO (Matlab) support" ON)
 option(WITH_INTERNAL_DOC "Compile doxygen internal documentation" OFF)
 option(WITH_TESTING "Build with unit tests" OFF)
 
