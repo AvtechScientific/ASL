82d7c9aeb1918870e5c2f40dc31953a12c0d67cc
Avtech Scientific <AvtechScientific@users.noreply.github.com>
2015-07-03 13:44:16 +0300

    Adding config.cmake

--- cmake/DefineFunctions.cmake
+++ cmake/DefineFunctions.cmake
@@ -1,12 +1,20 @@
-# Installs sublibraries - binaries and
-# public headers (preserving source tree structure)
+# Installs sublibraries - binaries and public headers
+# (preserving source tree structure)
 function(INSTALL_SUBLIB _SUBLIB _SUBLIB_PUBLIC_HEADERS)
-	# ToDo: add COMPONENT headers
+
+	set_target_properties(
+		${_SUBLIB} PROPERTIES VERSION ${ASL_VERSION}
+		SOVERSION ${ASL_VERSION_MAJOR}
+		INTERFACE_${_SUBLIB}_MAJOR_VERSION ${ASL_VERSION_MAJOR}
+		COMPATIBLE_INTERFACE_STRING ${ASL_VERSION_MAJOR}
+	)
+
 	install(TARGETS
-		${_SUBLIB}
+		${_SUBLIB} EXPORT ASLTargets
 		RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
 		LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
 		ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
+		INCLUDES DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/asl
 	)
 
 	# Install public headers preserving the source tree structure
@@ -15,11 +23,11 @@ function(INSTALL_SUBLIB _SUBLIB _SUBLIB_PUBLIC_HEADERS)
 		file(RELATIVE_PATH relative_path ${CMAKE_SOURCE_DIR}/src ${CMAKE_CURRENT_SOURCE_DIR}/${header})
 		# Extract directories of the relative path
 		get_filename_component(directories ${relative_path} DIRECTORY)
-		# ToDo: add COMPONENT libraries
 		install(FILES
 			${header}
 			DESTINATION
 			${CMAKE_INSTALL_INCLUDEDIR}/asl/${directories}
+			COMPONENT Devel
 		)
 	endforeach()
 endfunction(INSTALL_SUBLIB)
