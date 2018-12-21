49ded4c5deeac448d47fc6e5bfdc27e9d78205c6
Avtech Scientific <AvtechScientific@users.noreply.github.com>
2018-12-21 13:35:43 +0200

    Adding CTestConfig.cmake

---
+++ CTestConfig.cmake
@@ -0,0 +1,3 @@
+# This file is needed to enable creation of XML output (Testing/*/Test.xml)
+# without relying on the DartConfiguration.tcl file
+# see https://cmake.org/pipermail/cmake/2015-January/059742.html
